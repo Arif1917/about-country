4e077d5eac87c8bc17c1c535a8173d86e6295fe1
Arif1917 <mdarifhosen1917@gmail.com>
2025-04-19 08:24:20 +0600

    visited button added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css'
 
-const handleClick = ()=>{
-    console.log('handle click')
-}
 const Country = ({country}) => {
+    const [visited, setVisited]=useState(false)
+const handleClick = ()=>{
+    if(visited){
+        setVisited(false)
+    }
+    else{
+        setVisited(true)
+    }
+}
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-bg'}`}>
             <h2>Name:{country.name.common}</h2>
             <img src={country.flags.png} alt="" />
             <h2>Independent: {country.independent? 'free': 'not free'}</h2>
             <h2>Population: {country.population}</h2>
-            <button onClick={handleClick}>Non visited</button>
+        <button className={visited ? 'nont':'hello'} onClick={handleClick}>{visited ? 'visited':'non-visited'}</button>
         </div>
     );
 };
